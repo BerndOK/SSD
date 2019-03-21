7408375b83f66c1e4b1efabac321b16ea833d98c
Bernd OK <BerndOK@gmail.com>
2019-03-20 20:03:25 -0500

    03/20 late versions

--- JCA/src/jca/crypt/symmetric/AESDemo.java
+++ JCA/src/jca/crypt/symmetric/AESDemo.java
@@ -5,6 +5,7 @@ import javax.crypto.KeyGenerator;
 
 import java.security.Key;
 import java.security.SecureRandom;
+import java.util.Base64;
 
 public class AESDemo {
    public static void main(String args[]) throws Exception{
@@ -20,15 +21,15 @@ public class AESDemo {
       //Creating/Generating a key
       Key key = keyGen.generateKey();
       
-      System.out.println(key);      
       Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");      
       cipher.init(cipher.ENCRYPT_MODE, key);      
 
       String msg = new String("Hi how are you");
       byte[] bytes = cipher.doFinal(msg.getBytes());      
+      System.out.println(Base64.getEncoder().encodeToString(bytes));
+
+      cipher.init(cipher.DECRYPT_MODE, key); 
+      bytes = cipher.doFinal(bytes);      
       System.out.println(new String(bytes));
-      // cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-      cipher.init(cipher.DECRYPT_MODE, key);      
-      System.out.println(new String(cipher.doFinal(bytes)));
    }
 }
